cefe4084236a5a2c83fb7980bf29d5917a985dba
Abubakar Yusif <abubakaryusuf951@gmail.com>
2019-10-20 01:18:26 +0800

    Create Quiz_1.java
    done with quiz_1

---
+++ Quizes/Quiz_1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Analy {
+    public static void main(String[] args) {
+        int player, computer;
+        int rock = 1, paper = 2, sc = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Your choice");
+        player = input.nextInt();
+        computer = (int )(Math.random()*3);
+        if (computer == paper){
+            if (player == sc ){
+                System.out.println("player wins");
+            } else if (player == rock ){
+                System.out.println("Computer wins");
+            } else if (player == paper ){
+                System.out.println("Draw");
+            }
+        } if (computer ==rock){
+            if (player == paper ){
+                System.out.println("player wins");
+            } else if (player == sc ){
+                System.out.println("Computer wins");
+            } else if (player == rock ){
+                System.out.println("Draw");
+            }
+        } if (computer == sc){
+            if (player == rock ){
+                System.out.println("player wins");
+            } else if (player == paper ){
+                System.out.println("Computer wins");
+            } else if (player == sc ){
+                System.out.println("Draw");
+            }
+        }
+    }
+
+}
